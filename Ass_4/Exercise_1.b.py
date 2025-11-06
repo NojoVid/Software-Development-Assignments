13b71a9a03ca139bca40c264ed0e41d81c936f67
NojoVid <noavidacak1234@gmail.com>
2025-11-06 11:55:15 +0100

    Delete Ass_4 directory

--- Ass_4/Exercise_1.b.py
+++
@@ -1,6 +0,0 @@
-## Take <parts> (tuple) and gives part names
-
-parts = [(1, '500-1', 'Hammer', 2, 'Pieces'), (2, '503', 'Screwdriver', 3, 'Pieces'), (3, '555', 'Nail', 10, 'Pieces')]
-
-part_names = [name for item, part_number, name, quantity, unit in parts]
-print(part_names)
--- Ass_4/Exercise_2.py
+++
@@ -1,9 +0,0 @@
-## Takes <parts> (tuple) and creates a dictionary in form of <part_number: (quantity, unit)>
-
-inventory = {}
-parts = [(1, '500-1', 'Hammer', 2, 'Pieces'), (2, '503', 'Screwdriver', 3, 'Pieces'), (3, '555', 'Nail', 10, 'Pieces')]
-
-for item, part_number, name, quantity, unit in parts:
-    inventory[part_number] = (quantity, unit)
-
-print(inventory)
