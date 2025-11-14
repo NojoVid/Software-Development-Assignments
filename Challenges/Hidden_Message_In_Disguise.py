afa04651baa4dacdf59abe3d1e05888fa2619a99
NojoVid <noavidacak1234@gmail.com>
2025-11-14 13:22:42 +0100

    Add files via upload

---
+++ Challenges/Hidden_Message_In_Disguise.py
@@ -0,0 +1,31 @@
+## Encrypt the two hidden email adresses
+
+email_1 = 'rdUYjcfruqjUYrfnqAYrfnqPYhtr'
+email_2 = 'otmsPYitjAYrfnqUYzxPYhtr'
+
+def caesar_cypher(text):
+    
+    letters_low = list("abcdefghijklmnopqrstuvwxyz")
+    result = ''
+
+    for item in text:
+        if item.islower():
+            index = letters_low.index(item)
+            result += letters_low[index - 5]
+        else:
+            if item == 'P':
+                result += '.'
+            elif item == 'U':
+                result += '_'
+            elif item == 'A':
+                result += '@'
+            else:
+                pass
+
+    return result
+
+print('-' * len(email_1))
+print(caesar_cypher(email_1))
+print(caesar_cypher(email_2))
+print('-' * len(email_1))
+
